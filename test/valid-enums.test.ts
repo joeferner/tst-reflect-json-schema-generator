98ecbf21fbbcf83605140dab2a68b610d83441ef
Joe Ferner <joe@fernsroth.com>
2022-05-25 23:08:26 +0000

    wip

--- test/valid-enums.test.ts
+++ test/valid-enums.test.ts
@@ -1,10 +1,24 @@
 import { getType } from "tst-reflect";
 import { assertValidSchema } from "./utils";
+// import { Enum as EnumsComputeEnum } from "./valid/enums-compute/main";
+import { Enum as EnumsInitializedEnum } from "./valid/enums-initialized/main";
 import { MyObject as EnumsMemberMyObject } from "./valid/enums-member/main";
 import { Enum as EnumsNumberEnum } from "./valid/enums-number/main";
 import { Enum as EnumsStringEnum } from "./valid/enums-string/main";
 
 describe("valid-enums", () => {
+  // TODO evaluate literal expressions to get values
+  // test("enums-compute", () => {
+  //   assertValidSchema("valid/enums-compute", getType<EnumsComputeEnum>());
+  // });
+
+  test("enums-initialized", () => {
+    assertValidSchema(
+      "valid/enums-initialized",
+      getType<EnumsInitializedEnum>()
+    );
+  });
+
   test("enums-member", () => {
     assertValidSchema("valid/enums-member", getType<EnumsMemberMyObject>());
   });
